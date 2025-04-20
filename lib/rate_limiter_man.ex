db9fab0396d89062e0df7727efb88855e4f53787
Nicholas Moen <arcanemachine@gmail.com>
2025-04-19 22:01:29 -0600

    Create project

---
+++ lib/rate_limiter_man.ex
@@ -0,0 +1,18 @@
+defmodule RateLimiterMan do
+  @moduledoc """
+  Documentation for `RateLimiterMan`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RateLimiterMan.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rate_limiter_man_test.exs
@@ -0,0 +1,8 @@
+defmodule RateLimiterManTest do
+  use ExUnit.Case
+  doctest RateLimiterMan
+
+  test "greets the world" do
+    assert RateLimiterMan.hello() == :world
+  end
+end
